0b9feaf24dfb19c204b13f4bb0d18cdfaebcde89
mh <observermatth@gmail.com>
2023-08-19 01:48:21 -0300

    fix(doom-modeline-core.el): fix a warning bug with
      `doom-modeline-propertize-icon` (was in the tree, but I deleted before the
        previous `fork-master` merger)

--- doom-modeline-core.el
+++ doom-modeline-core.el
@@ -1223,14 +1223,7 @@ The face should be the first attribute, or the font family may be overridden.
 So convert the face \":family XXX :height XXX :inherit XXX\" to
 \":inherit XXX :family XXX :height XXX\".
 See https://github.com/seagle0128/doom-modeline/issues/301."
-  (if (doom-modeline-icon-displayable-p)
-      (when-let ((props (get-text-property 0 'face icon)))
-        (when (listp props)
-          (cl-destructuring-bind (&key family height inherit &allow-other-keys) props
-            (propertize icon 'face `(:inherit (doom-modeline ,(or face inherit props))
-                                     :family  ,(or family "")
-                                     :height  ,(or height 1.0))))))
-    (propertize icon 'face `(:inherit (doom-modeline ,face)))))
+  (propertize icon 'face `(:inherit (doom-modeline ,face))))
 
 (defun doom-modeline-icon (text &rest args)
   "Display icon of ICON-NAME with ARGS in mode-line.
